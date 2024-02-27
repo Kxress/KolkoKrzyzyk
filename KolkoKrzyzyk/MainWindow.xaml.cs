6c1a895123fe0818096b6f55032cf1aa6c346761
Kxress <gymystik@gmail.com>
2024-02-27 14:00:26 +0100

    init

---
+++ KolkoKrzyzyk/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using KolkoKrzyzyk.states;
+
+namespace KolkoKrzyzyk
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Game game_ = new Game();
+            mainFrame.Content = game_;
+        }
+    }
+}
---
+++ KolkoKrzyzyk/states/Game.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KolkoKrzyzyk.states
+{
+    public partial class Game : Page
+    {
+        public string krzyzyk = "\x2715";
+        public string kolko = "\x25EF";
+        public bool czyKrzyzyk = false;
+
+        public Game()
+        {
+            InitializeComponent();
+
+            // przypisywanie eventu btnClick wszystkim przyciskom
+            foreach (UIElement element in plansza.Children)
+            {
+                if (element is Button button)
+                {
+                    button.Click += new RoutedEventHandler(btnClick);
+                }
+            }
+        }
+
+        // naprzemienna zmiana symbolu
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            var nacisnietyPrzycisk = e.Source as Button;
+
+            if (!czyKrzyzyk)
+            {
+                nacisnietyPrzycisk.Content = krzyzyk;
+                czyKrzyzyk = true;
+            }
+            else
+            {
+                nacisnietyPrzycisk.Content = kolko;
+                czyKrzyzyk = false;
+            }
+        }
+    }
+}
