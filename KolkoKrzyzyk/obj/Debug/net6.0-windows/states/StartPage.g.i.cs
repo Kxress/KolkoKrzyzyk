96359aa11d42157216e96e69a3554bc4d6320236
Kxress <gymystik@gmail.com>
2024-03-18 11:57:05 +0100

    added .gitignore

--- KolkoKrzyzyk/obj/Debug/net6.0-windows/states/StartPage.g.i.cs
+++ KolkoKrzyzyk/obj/Debug/net6.0-windows/states/StartPage.g.i.cs
@@ -79,13 +79,13 @@ namespace KolkoKrzyzyk.states {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.26.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/KolkoKrzyzyk;V1.0.0.0;component/states/startpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KolkoKrzyzyk;component/states/startpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\states\StartPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -95,7 +95,7 @@ namespace KolkoKrzyzyk.states {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.26.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
